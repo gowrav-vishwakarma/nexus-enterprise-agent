ca4f7553cad63263d09e697a99ed63f3a3e09a40
Gowrav Vishwakarma <gowravvishwkarma@gmail.com>
2026-07-31 18:52:30 +0530

    chore: release v0.3.2

--- nexus/__init__.py
+++ nexus/__init__.py
@@ -66,7 +66,7 @@ from nexus.multiagent.results import AgentGroupResult
 from nexus.orchestration import OrchestrationManifest, OrchestrationRuntime
 from nexus.events import EventEmitted, NexusEventEmitter, NexusEventType, NexusEvent
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 __all__ = [
     # Version
